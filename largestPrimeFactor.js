2639c554a233da967938620e34ff18d9a0a1a8eb
Steven Cable <steven.cable12@gmail.com>
2016-11-19 13:36:37 -1000

    all solved

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,20 @@
-exports.largestPrimeFactor = function(n){
-  var primeNumber = 0;
-  // do your work here
+var primeNumber = 0;
+  function isPrime(num){
+    for(var i =2; i<=num/2; i++){
+      if( num % i ===0){
+        return false;
+      }
+    }
+    return true;
+  }
 
+
+exports.largestPrimeFactor = function(n){
+  
+for (var i = 2, len=n; i < len; i++) {
+  if (n%i === 0 && isPrime(i)) {
+    primeNumber = i;
+  }
+}
   return primeNumber;
 };
